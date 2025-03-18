4c0d3b6bbf54d9440469669695454a0afa2b08e8
Xumoyun <rabbimovxumoyun22@gmail.com>
2025-03-18 23:32:37 +0500

    changes are added

--- src/components/Blog/SingleBlog.tsx
+++ src/components/Blog/SingleBlog.tsx
@@ -5,22 +5,19 @@ import Link from "next/link";
 const SingleBlog = ({ blog }: { blog: Blog }) => {
   const { title, image, paragraph, tags, link } = blog;
   return (
-    <>
+    <Link href={link}>
       <div className="group relative overflow-hidden rounded-sm bg-white shadow-one duration-300 hover:shadow-two dark:bg-dark dark:hover:shadow-gray-dark">
-        <Link href={link} className="relative block aspect-[37/22] w-full">
+        <div className="relative block aspect-[37/22] w-full">
           <span className="absolute right-6 top-6 z-20 inline-flex items-center justify-center rounded-full bg-primary px-4 py-2 text-sm font-semibold capitalize text-white">
             {tags[0]}
           </span>
           <Image src={image} alt="image" fill />
-        </Link>
+        </div>
         <div className="p-6 sm:p-8 md:px-6 md:py-8 lg:p-8 xl:px-5 xl:py-8 2xl:p-8">
           <h3>
-            <Link
-              href={link}
-              className="mb-4 block text-xl font-bold text-black hover:text-primary dark:text-white dark:hover:text-primary sm:text-2xl"
-            >
+            <div className="mb-4 block text-xl font-bold text-black group-hover:text-primary dark:text-white dark:hover:text-primary sm:text-2xl">
               {title}
-            </Link>
+            </div>
           </h3>
           <p className="mb-6 border-b border-body-color border-opacity-10 pb-6 text-base font-medium text-body-color dark:border-white dark:border-opacity-10">
             <span className="line-clamp-4 ">{paragraph}</span>
@@ -48,7 +45,7 @@ const SingleBlog = ({ blog }: { blog: Blog }) => {
           </div> */}
         </div>
       </div>
-    </>
+    </Link>
   );
 };
 
